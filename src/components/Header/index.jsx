1977c8db7c3cdc7d021b7259305790cdd82e58e0
chuper <chuper@gmx.fr>
2023-06-03 15:02:27 +0200

    #

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,22 @@
-import logo from '../../assets/LOGO.png'
-import { NavLink } from 'react-router-dom'
-import '../../styles/Header.css'
+import logo from '../../assets/LOGO.png';
+import { NavLink } from 'react-router-dom';
+import '../../styles/Header.css';
 function Header() {
-    return (
-        <header>
-            <img src={logo} alt='logo kasa' className='header-logo' />
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/">Accueil</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/About">A propos</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <img src={logo} alt="logo kasa" className="header-logo" />
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/kasa/">Accueil</NavLink>
+          </li>
+          <li>
+            <NavLink to="/About">A propos</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- src/router.jsx
+++ src/router.jsx
@@ -1,26 +1,23 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
-import Home from './pages/Home'
-import About from './pages/About'
-import Logement from './pages/Logement'
-import Error from './components/Error'
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Logement from './pages/Logement';
+import Error from './components/Error';
 
 const router = createBrowserRouter([
-    {
-        element: <App />,
-        children: [
-          { path: "/", element: <Home /> },
-          {
-            element: <Logement />,
-            path:"/logement/:logementId",
-          },
-          { path: "/about", element: <About /> },
-          { path: "*", element: <Error /> },
-        ],
-      }
-]
-)
+  {
+    element: <App />,
+    children: [
+      { path: '/kasa/', element: <Home /> },
+      {
+        element: <Logement />,
+        path: '/logement/:logementId',
+      },
+      { path: '/about', element: <About /> },
+      { path: '*', element: <Error /> },
+    ],
+  },
+]);
 
-
-
-export default router
+export default router;
